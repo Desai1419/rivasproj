a2a0b2fb1d6cdae977eaaaf7bc2f4a9fa5b41a10
Desai1419 <53499783+Desai1419@users.noreply.github.com>
2019-10-09 09:57:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <html>
 <body>
 
-<p id="one"></p>
-<button onclick="random()">Random Number from 1-10</button>
+ <center><p id="one"></p></center>
+ <center><button onclick="random()">Random Number from 1-10</button></center>
 
 <script>
  function random(){
