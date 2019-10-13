f2daaa56549151726faa736db3531a623ae53eca
Desai1419 <53499783+Desai1419@users.noreply.github.com>
2019-10-13 21:39:20 +0845

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <html>
 <body>
+ body {
+ background-image: url("https://images.immediate.co.uk/production/volatile/sites/4/2018/08/GettyImages-667585631use-a3986be.jpg?quality=45&crop=6px,65px,928px,399px&resize=960,413");
+}
 
  <center><p id="one"></p></center>
  <center><button onclick="random()">Random Numbers</button></center>
