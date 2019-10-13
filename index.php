d9c779717f9fde39fed2e53a462715788c9d2089
Desai1419 <53499783+Desai1419@users.noreply.github.com>
2019-10-13 21:46:07 +0845

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
     body {
  background-image: url("https://images.immediate.co.uk/production/volatile/sites/4/2018/08/GettyImages-667585631use-a3986be.jpg?quality=45&crop=6px,65px,928px,399px&resize=960,413");
   background-repeat: no-repeat;
-  background-size: auto;
    }
   </style>
  </head>
