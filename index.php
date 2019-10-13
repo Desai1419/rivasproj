7744360c669624cf2f20fb0b31a2328a76fd4fc1
Desai1419 <53499783+Desai1419@users.noreply.github.com>
2019-10-13 21:44:57 +0845

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,14 @@
   <style>
     body {
  background-image: url("https://images.immediate.co.uk/production/volatile/sites/4/2018/08/GettyImages-667585631use-a3986be.jpg?quality=45&crop=6px,65px,928px,399px&resize=960,413");
-}
+  background-repeat: no-repeat;
+  background-size: auto;
+   }
   </style>
  </head>
 <body>
 
- <center><p id="one"></p></center>
+ <center><font color="white"><p id="one"></p></font></center>
  <center><button onclick="random()">Random Numbers</button></center>
 
 <script>
