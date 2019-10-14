6a49cea3783622cd8f6542a013912f7ab281d642
Desai1419 <53499783+Desai1419@users.noreply.github.com>
2019-10-14 09:59:37 +0800

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
  <center><font color="white"><p id="one"></p></font></center>
  <center><button onclick="random()">Press to Reveal Random Numbers</button></center>
- <center><font color="white"><h2>TEST YOUR LUCK AND GET 42069 TO 69420 TO GET NOICE</h2></font></center>
+ <center><font color="white"><h2>TEST YOUR LUCK AND GET 42069 OR 69420 TO PASS THIS SEM</h2></font></center>
 
 <script>
  function random(){
